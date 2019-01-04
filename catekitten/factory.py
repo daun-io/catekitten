57920df98f28c89ace5312c07fb3f60235d736b3
nyanye <iam@nyanye.com>
2019-01-04 17:11:07 +0900

    add simple model factory

---
+++ catekitten/factory.py
@@ -0,0 +1,10 @@
+from catekitten.network import YoonKimCNN, YoonKimCNNv2, YoonKimCNNv3
+from catekitten.han import HAN
+
+
+NETWORKS = {
+    'YoonKimCNN': YoonKimCNN,
+    'YoonKimCNNv2': YoonKimCNNv2,
+    'YoonKimCNNv3': YoonKimCNNv3,
+    'HAN': HAN,
+}
